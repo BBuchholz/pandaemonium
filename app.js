02ba54b518b2480d1fb22de68fd7d9787d6b1fc0
EC2 Default User <ec2-user@ip-172-31-32-110.us-east-2.compute.internal>
2020-07-03 00:57:26 +0000

    modify server to serve static index.html page

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express        = require( 'express' );
-var http           = require( 'http' );
+var express        = require('express');
+var http           = require('http');
 
 var app            = express();
+var path           = require('path');
+
 app.set( 'port', process.env.PORT || 3001 );
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    //res.send('Hello World');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 http.createServer( app ).listen( app.get( 'port' ), function (){
