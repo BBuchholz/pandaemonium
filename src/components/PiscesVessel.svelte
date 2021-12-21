7a628224b0db361e7b09a56f59f12270d3d6a6b7
ENTOMDT <entomdt@gmail.com>
2021-12-21 17:56:44 -0600

    Modify remaning buttons to be uniform

--- src/components/PiscesVessel.svelte
+++ src/components/PiscesVessel.svelte
@@ -64,4 +64,23 @@
     color: #ffffff;
   }
 
+  
+  
+button {
+  font-size: 15px;
+  line-height: 15px;
+  padding: 8px 22px 9px 22px;
+  box-sizing: border-box;
+  font-weight: 700;
+  border: solid 3px white;
+  color: white;
+  background: transparent;
+  border-radius: 30px;
+  transition: all ease-out .2s;
+  cursor: pointer;
+  outline: none;
+  animation: buttonPulse .2s infinite ease-in-out;
+}
+
+
 </style>
