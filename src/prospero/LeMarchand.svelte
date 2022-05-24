6a3244f01f22ffc387d04d955fe3f3b695255af8
ENTOMDT <entomdt@gmail.com>
2022-05-24 10:36:43 -0500

    Add Prospero Configuration to LeMarchand

--- src/prospero/LeMarchand.svelte
+++ src/prospero/LeMarchand.svelte
@@ -1,5 +1,30 @@
 <script>
   
+  // TODO: I want this for passphrase prospero, which
+  // will bring to the same place as lament, but will
+  // display this quote in stead... 
+  const prosperosEpilogue = `
+           Now my charms are all o'erthrown,
+           And what strength I have's mine own,
+           Which is most faint: now, 'tis true,
+           I must be here confined by you,
+           Or sent to Naples. Let me not,
+           Since I have my dukedom got
+           And pardon'd the deceiver, dwell
+           In this bare island by your spell;
+           But release me from my bands
+           With the help of your good hands:
+           Gentle breath of yours my sails
+           Must fill, or else my project fails,
+           Which was to please. Now I want
+           Spirits to enforce, art to enchant,
+           And my ending is despair,
+           Unless I be relieved by prayer,
+           Which pierces so that it assaults
+           Mercy itself and frees all faults.
+           As you from crimes would pardon'd be,
+           Let your indulgence set me free.`;
+
   import { getContext } from 'svelte';
 
   import { getNotificationsContext } from 'svelte-notifications';
