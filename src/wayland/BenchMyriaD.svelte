f7135d9d625b26fd71aa8799a4ccb518458226d2
ENTOMDT <entomdt@gmail.com>
2022-08-08 10:13:19 -0500

    Add OpRoom To Wayland

---
+++ src/wayland/BenchMyriaD.svelte
@@ -0,0 +1,60 @@
+<script>
+  
+  import { getContext } from 'svelte';
+
+  import { getNotificationsContext } from 'svelte-notifications';
+  const { addNotification } = getNotificationsContext();
+
+  import {  
+    passPhrase,
+    devMode, 
+  } from '../stores.js';
+
+  function notifyWithoutExpire(textValue){
+    addNotification({
+      position: 'bottom-right',
+      text: textValue,
+      type: 'error',
+      description: 'lorem ipsum',
+      disableButtons: false,
+      disableDescription: true
+    });
+  }
+
+  function handleOpRoomClick(){
+    $passPhrase = 'Channard';
+  }
+
+</script>
+
+<div class='bench'>
+  
+    <div class="bench-item">
+      <a 
+        href="#opRoom"
+        on:click={handleOpRoomClick}
+      > 
+        Operati(ons/ng) Room
+      </a>
+    </div>
+  
+</div>
+
+<style>
+
+.bench {
+  border: solid;
+  border-radius: 10px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+}
+
+.bench-item {
+  margin: 10px;
+  padding: 10px;
+  border: thin solid;
+  border-radius: 10px;
+}
+
+</style>
---
+++ src/wayland/OpRoom.svelte
@@ -0,0 +1,49 @@
+<script>
+
+  import KnechtController from '../myriad/KnechtController.js';
+  const magisterLudi = KnechtController();
+
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+  
+  import { 
+    passPhrase,
+    affinityAnchor,
+    affinityKeys,
+  } from '../stores.js';
+
+  import { getNotificationsContext } from 'svelte-notifications';
+  const { addNotification } = getNotificationsContext();
+  
+  function notifyPassPhraseFound(){
+    addNotification({
+      position: 'top-right',
+      text: 'passPhrase Found',
+      type: 'error',
+      description: 'passPhrase: ' + $passPhrase,
+      removeAfter: 10000,
+    });
+  }
+
+
+</script>
+
+  <div class="op-room">
+
+    OP ROOM GOES HERE
+
+  </div> 
+
+
+<style>
+
+.op-room {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+
+
+</style>
